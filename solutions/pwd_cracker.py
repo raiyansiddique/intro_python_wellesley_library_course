0803a02ea0e84e48f5e9cdba13f1586bf61750b1
evlock <elockwood@olin.edu>
2023-02-16 18:09:22 -0500

    Write prompt for password_cracker

--- solutions/pwd_cracker.py
+++ solutions/pwd_cracker.py
@@ -1,3 +1,10 @@
+# Write a Python script that uses the random library to generate 
+# random character combinations and simulate keyboard inputs. 
+# The script prompts the user for a password, and then uses a while loop to 
+# generate random character combinations until the correct password is found. 
+# The generated password is compared to the user's input password using a list comparison,
+# and if a match is found, the script prints the password to the console.
+
 import random
 import pyautogui
 
